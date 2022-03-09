1b3a58f2812eceeb759b94218c5ede2eeb01f11d
Rafael Mançan <rafael.mancan@gmail.com>
2022-03-09 15:36:06 -0300

    creating task list and input to add

---
+++ src/components/tasks/TaskItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-list-item>
+    <template v-slot:default="{ active }">
+      <v-list-item-action>
+        <v-checkbox :input-value="active"></v-checkbox>
+      </v-list-item-action>
+
+      <v-list-item-content>
+        <v-list-item-title>{{task.name}}</v-list-item-title>
+      </v-list-item-content>
+    </template>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'TaskItem',
+  props: ['task']
+}
+</script>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,10 +1,78 @@
 <template>
-  <h1>Tasks Page</h1>
-</template>
+  <v-list
+      flat
+      subheader
+    >
+      <v-list-item-group
+        multiple
+        active-class=""
+      >
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-text-field
+            label="Qual é a sua tarefa?"
+            outlined
+            dense
+            clearable
+            v-model="newTask"
+            @keyup.enter="handleAddTask"
+          ></v-text-field>
+        </v-col>
+        <div
+          v-for="task in tasks"
+          :key="task.id"
+        >
+          <TaskItem
+            :task="task"
+          />
+        </div>
+      </v-list-item-group>
+    </v-list></template>
 
 <script>
+import TaskItem from '../components/tasks/TaskItem.vue'
 
 export default {
-  name: 'TasksView'
+  name: 'TasksView',
+  components: {
+    TaskItem
+  },
+  data () {
+    return {
+      newTask: null,
+      tasks: [
+        {
+          id: 1,
+          name: 'Task 1',
+          completed: false
+        },
+        {
+          id: 2,
+          name: 'Task 2',
+          completed: false
+        },
+        {
+          id: 3,
+          name: 'Task Completed',
+          completed: true
+        }
+      ]
+    }
+  },
+  methods: {
+    handleAddTask () {
+      if (this.newTask) {
+        this.tasks.push({
+          id: this.tasks.length + 1,
+          name: this.newTask,
+          completed: false
+        })
+        this.newTask = null
+      }
+    }
+  }
 }
 </script>
